8899710e3ffa6ca8d1dcc9a04e09d50e29151543
whohensee <106775295+whohensee@users.noreply.github.com>
2025-10-29 13:03:01 -0700

    Begin building components

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children?.()}
+
+<style>
+	:global(*),
+	:global(*::before),
+	:global(*::after) {
+		box-sizing: border-box;
+	}
+
+	:global(*) {
+		margin: 0;
+		padding: 0;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Header from './Header.svelte';
+</script>
+
+<Header />
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation, okay
+	boyo?
+</p>
