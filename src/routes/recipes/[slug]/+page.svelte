afb3db77ff5edd7ec16c9689e736749d39e2d8e8
whohensee <106775295+whohensee@users.noreply.github.com>
2025-10-30 12:16:27 -0700

    Basic formatting for recipe page

--- src/routes/recipes/[slug]/+page.svelte
+++ src/routes/recipes/[slug]/+page.svelte
@@ -1,13 +1,31 @@
 <script>
 	let { data } = $props();
+	import Instructions from './Instructions.svelte';
+	import Ingredients from './Ingredients.svelte';
 </script>
 
 {#if data.recipe}
-	<h1>{data.recipe.title}</h1>
-	<h2>Ingredients</h2>
-	<p>{data.recipe.ingredients}</p>
-	<h2>Instructions</h2>
-	<p>{data.recipe.instructions}</p>
+	<div class="wrapper">
+		<h1>{data.recipe.title}</h1>
+		<div class="content">
+			<Instructions instructionText={data.recipe.instructions} />
+			<Ingredients ingredientText={data.recipe.ingredients} />
+		</div>
+	</div>
 {:else}
 	<h1>There's no recipe here!</h1>
 {/if}
+
+<style>
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.content {
+		display: flex;
+		justify-content: space-between;
+		max-width: 1000px;
+		width: 100%;
+	}
+</style>
---
+++ src/routes/recipes/[slug]/Ingredients.svelte
@@ -0,0 +1,27 @@
+<script>
+	let { ingredientText } = $props();
+	import { theme } from '$lib/themes/basicTheme';
+</script>
+
+<div class="content" style="--bgcolor: {theme.header_color}">
+	<h2>Ingredients</h2>
+	<p>{ingredientText}</p>
+</div>
+
+<style>
+	.content {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		align-items: center;
+
+		max-width: 490px;
+		width: 100%;
+
+		background-color: var(--bgcolor);
+	}
+
+	p {
+		width: 100%;
+	}
+</style>
