cc9e287bc5a594defb6812b1c3754b8a5ceee0d3
whohensee <106775295+whohensee@users.noreply.github.com>
2025-10-30 08:22:11 -0700

    Create fake recipe data

---
+++ src/lib/data/recipes.js
@@ -0,0 +1,37 @@
+export const recipes = [
+	{
+		title: 'Masala Lentils',
+		slug: 'masala-lentils',
+		instructions: 'lots more stuff',
+		imgURL: '$lib/assets/favicon.svg',
+		ingredients: 'lots of stuff'
+	},
+	{
+		title: 'Masala Lentils',
+		slug: 'masala-lentils',
+		instructions: 'lots more stuff',
+		imgURL: '$lib/assets/favicon.svg',
+		ingredients: 'lots of stuff'
+	},
+	{
+		title: 'Masala Lentils',
+		slug: 'masala-lentils',
+		instructions: 'lots more stuff',
+		imgURL: '$lib/assets/favicon.svg',
+		ingredients: 'lots of stuff'
+	},
+	{
+		title: 'Masala Lentils',
+		slug: 'masala-lentils',
+		instructions: 'lots more stuff',
+		imgURL: '$lib/assets/favicon.svg',
+		ingredients: 'lots of stuff'
+	},
+	{
+		title: 'Masala Lentils',
+		slug: 'masala-lentils',
+		instructions: 'lots more stuff',
+		imgURL: '$lib/assets/favicon.svg',
+		ingredients: 'lots of stuff'
+	}
+];
