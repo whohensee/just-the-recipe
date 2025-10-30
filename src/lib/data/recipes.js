629fc672df7c136604dcdee118a917ac4f2a6521
whohensee <106775295+whohensee@users.noreply.github.com>
2025-10-30 08:57:35 -0700

    Make recipe names unique

--- src/lib/data/recipes.js
+++ src/lib/data/recipes.js
@@ -9,29 +9,29 @@ export const recipes = [
 		ingredients: 'lots of stuff'
 	},
 	{
-		title: 'Masala Lentils',
-		slug: 'masala-lentils',
+		title: 'Rice and Beans',
+		slug: 'rice-and-beans',
 		instructions: 'lots more stuff',
 		imgURL: imgURL,
 		ingredients: 'lots of stuff'
 	},
 	{
-		title: 'Masala Lentils',
-		slug: 'masala-lentils',
+		title: 'Instant Pot Quinoa',
+		slug: 'instant-pot-quinoa',
 		instructions: 'lots more stuff',
 		imgURL: imgURL,
 		ingredients: 'lots of stuff'
 	},
 	{
-		title: 'Masala Lentils',
-		slug: 'masala-lentils',
+		title: 'Instant Pot Rice',
+		slug: 'instant-pot-rice',
 		instructions: 'lots more stuff',
 		imgURL: imgURL,
 		ingredients: 'lots of stuff'
 	},
 	{
-		title: 'Masala Lentils',
-		slug: 'masala-lentils',
+		title: 'Potato Lentil Soup',
+		slug: 'potato-lentil-soup',
 		instructions: 'lots more stuff',
 		imgURL: imgURL,
 		ingredients: 'lots of stuff'
