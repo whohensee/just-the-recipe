ad8ce3af2463bb156bc07764f6b120ee338cec96
whohensee <106775295+whohensee@users.noreply.github.com>
2025-11-05 11:26:02 -0800

    Add delete button to recipes

--- src/routes/recipes/[slug]/+page.server.js
+++ src/routes/recipes/[slug]/+page.server.js
@@ -1,9 +1,11 @@
 import { connectToDB } from '$lib/database/db';
+import { DELETE_PWD } from '$env/static/private';
+import { redirect, fail } from '@sveltejs/kit';
 
 export async function load({ params }) {
 	const client = await connectToDB();
 	const q1 = await client.query(
-		'SELECT title, slug, instructions, imgurl, ingredients FROM recipes'
+		'SELECT id, title, slug, instructions, imgurl, ingredients FROM recipes'
 	);
 	let recipes = q1.rows ?? [];
 	client.release();
@@ -14,3 +16,45 @@ export async function load({ params }) {
 		recipe
 	};
 }
+
+export const actions = {
+	delete: async ({ request }) => {
+		const data = await request.formData();
+
+		// check the pwd
+		if (data.get('pwd') != DELETE_PWD) {
+			console.log('expected: ' + DELETE_PWD + '\nreceived: ' + data.get('pwd'));
+			throw new Error('Wrong deletion password');
+		}
+
+		if (!data.get('recipe-id')) {
+			throw new Error('No recipe id');
+		}
+
+		const client = await connectToDB();
+		const q = 'DELETE FROM recipes WHERE id = $1';
+		const values = [data.get('recipe-id')];
+
+		try {
+			await client.query('BEGIN');
+
+			console.log('Trying to delete');
+			const resp = await client.query(q, values);
+
+			if (resp.rowCount != 1) {
+				throw new Error('Found ' + resp.rowCount + ' rows instead of 1');
+			}
+			await client.query('COMMIT');
+		} catch (error) {
+			await client.query('ROLLBACK');
+			console.log('Returning fail');
+			return fail(422, {
+				title: values[0],
+				error: error.message
+			});
+		} finally {
+			client.release();
+		}
+		redirect(303, '/');
+	}
+};
--- src/routes/recipes/[slug]/+page.svelte
+++ src/routes/recipes/[slug]/+page.svelte
@@ -4,6 +4,16 @@
 	import Instructions from './Instructions.svelte';
 	import Ingredients from './Ingredients.svelte';
 	import { theme } from '$lib/themes/basicTheme';
+	let confirmDelete = $state(false);
+	function deleteButton() {
+		if (!confirmDelete) {
+			confirmDelete = true;
+			return;
+		} else {
+			document.getElementById('deleteform').submit();
+		}
+	}
+	let deletion_pwd = $state('');
 </script>
 
 {#if data.recipe}
@@ -12,6 +22,13 @@
 		<a href={resolve('/recipes/update/[slug]', { slug: data.recipe.slug })}>
 			<button type="button" class="update">Update</button>
 		</a>
+		<form method="POST" action="?/delete" class="deletion" id="deleteform">
+			<input type="hidden" name="recipe-id" value={data.recipe.id} />
+			{#if confirmDelete}
+				<input type="text" name="pwd" value={deletion_pwd} autocomplete="off" />
+			{/if}
+			<button type="button" class="delete" onclick={deleteButton}>Delete</button>
+		</form>
 		<div class="content">
 			<Instructions instructions={data.recipe.instructions} />
 			<Ingredients ingredients={data.recipe.ingredients} />
