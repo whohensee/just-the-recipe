2f467e6797505278200843ef5c9a9d892474330b
whohensee <106775295+whohensee@users.noreply.github.com>
2025-11-05 14:11:37 -0800

    Very lame workaround for railway private network

--- src/lib/database/test-db.js
+++ src/lib/database/test-db.js
@@ -1,18 +1,16 @@
 import { Pool } from 'pg';
 import 'dotenv/config';
 
+console.log('trying the pool');
 const pool = new Pool({
-	database: process.env.R_DB_DBNAME,
-	user: process.env.R_DB_USER,
-	host: process.env.R_DB_HOST,
-	port: process.env.R_DB_PORT,
-	password: process.env.R_DB_PWD
+	connectionString: process.env.RAILWAY_DB_CONN_STRING
 });
 
+console.log('Made the pool');
 const client = await pool.connect();
 
 let q = 'SELECT 1';
-const resp = client.query(q);
+const resp = await client.query(q);
 console.log(resp);
 
 client.release();
