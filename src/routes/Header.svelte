79690cfdf3dacd51a62b50cbd5a8a0895d3540d2
whohensee <106775295+whohensee@users.noreply.github.com>
2025-11-05 12:43:23 -0800

    Minor Edit

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -8,7 +8,7 @@
 		<div class="content">
 			<h1><a href={resolve('/')}>Just The Recipe</a></h1>
 			<h2><a href={resolve('/recipes/create')}>New Recipe</a></h2>
-			<p>A site by William Hohensee</p>
+			<!-- <p>A site by William Hohensee</p> -->
 		</div>
 	</nav>
 </div>
