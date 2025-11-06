72669718250e8ad8c788aa5f33588562136777c5
whohensee <106775295+whohensee@users.noreply.github.com>
2025-11-06 13:18:17 -0800

    Add error page for unhandled errors

---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { theme } from '$lib/themes/basicTheme';
+</script>
+
+<div class="wrapper">
+	<div class="content" style="--bg-color: {theme.header_color}">
+		<h1>Sorry, something went wrong.</h1>
+		<h2>Please navigate back to the home page and don't do that again!</h2>
+	</div>
+</div>
+
+<style>
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.content {
+		background-color: var(--bg-color);
+		max-width: 1000px;
+		width: 100%;
+		text-align: center;
+	}
+</style>
