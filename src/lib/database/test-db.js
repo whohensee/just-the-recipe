602b16cb5af65a02e3eea23b4fffea578e0c7ed0
whohensee <106775295+whohensee@users.noreply.github.com>
2026-03-24 09:41:41 -0700

    Temp changes to connect to local db

--- src/lib/database/test-db.js
+++ src/lib/database/test-db.js
@@ -3,15 +3,19 @@ import 'dotenv/config';
 
 console.log('trying the pool');
 const pool = new Pool({
-	connectionString: process.env.RAILWAY_DB_CONN_STRING
+	// connectionString: process.env.RAILWAY_DB_CONN_STRING
+	connectionString: process.env.TEST_DB_CONNSTRING
 });
 
 console.log('Made the pool');
 const client = await pool.connect();
+console.log('Connected the client.');
 
 let q = 'SELECT 1';
 const resp = await client.query(q);
 console.log(resp);
 
 client.release();
+console.log('Released the client');
 pool.end();
+console.log('Closed the pool');
